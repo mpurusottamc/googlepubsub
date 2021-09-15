258d6bbc8995680b4b7e0cc356c8161beee93d23
Purusottam <mpurusottamc@gmail.com>
2021-09-15 16:53:09 -0700

    fixed google pubsub library usage

--- src/subscriber.js
+++ src/subscriber.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash');
 const Logger = require('./logger');
-const PubSub = require('./pubsub');
+const PubSubClient = require('./pubsub');
 
 function Subscriber(config, logger) {
 	let self = {};
@@ -11,25 +11,34 @@ function Subscriber(config, logger) {
 		self.config = config;
 		self.logger = logger;
 
-		self.pubsub = new PubSub({
-			project: self.config.project,
-			pubsub: self.config.pubsub
-		}, self.logger);
-	}
+		self.pubsub = new PubSubClient(
+			{
+				project: self.config.project,
+				pubsub: self.config.pubsub,
+			},
+			self.logger
+		);
+	};
 
 	self.attachListener = function (subscription) {
 		self.subscription = subscription;
-		self.pubsub.subscribe(subscription, self.messageHandler, self.errorHandler);
-	}
+		self.pubsub.subscribe(
+			subscription,
+			self.messageHandler,
+			self.errorHandler
+		);
+	};
 
 	self.errorHandler = function (error) {
 		console.error(`ERROR: ${error} - ${error.stack}`);
 
-		console.log(`reattaching listener to subscription - ${self.subscription}`);
+		console.log(
+			`reattaching listener to subscription - ${self.subscription}`
+		);
 
 		// Reattach after couple of seconds - 5
 		self.attachListener(self.subscription);
-	}
+	};
 
 	self.messageHandler = function (message) {
 		console.log(`Received a message for processing ${message.id}:`);
@@ -38,7 +47,7 @@ function Subscriber(config, logger) {
 
 		self.logger.log({
 			status: 'processing',
-			message: `received a message for processing ${message.data}`
+			message: `received a message for processing ${message.data}`,
 		});
 
 		const data = JSON.parse(message.data);
@@ -50,7 +59,7 @@ function Subscriber(config, logger) {
 
 			console.log(`message acknowledged`);
 		}, data.timeout);
-	}
+	};
 
 	self.init(config, logger);
 

e32e67814fed66f41bb6ffda250b05f0a71b60b8
Purusottam <mpurusottamc@gmail.com>
2021-09-15 12:49:28 -0700

    library updates

--- src/index.js
+++ src/index.js
@@ -36,56 +36,65 @@ process.on('SIGTERM', function () {
 
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-	extended: true
-})); // support encoded bodies
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+); // support encoded bodies
 
 app.get('/', (req, res) => {
 	console.log('server', 'In am running ...');
 
 	res.status(200).send({
-		status: 'success'
+		status: 'success',
 	});
 });
 
-
 const logger = new Logger({
 	project: config.Google.project,
-	logger: config.Google.logger
+	logger: config.Google.logger,
 });
-
 
 app.route('/:topic/publish').post((req, res) => {
 	const message = req.body;
 
-	const publisher = new Publisher({
-		project: config.Google.project,
-		pubsub: config.Google.pubsub
-	}, logger);
+	const publisher = new Publisher(
+		{
+			project: config.Google.project,
+			pubsub: config.Google.pubsub,
+		},
+		logger
+	);
 
-	publisher.publishMessage(message, req.params.topic).then(() => {
-		console.log(`published message successfully`);
+	publisher
+		.publishMessage(message, req.params.topic)
+		.then(() => {
+			console.log(`published message successfully`);
 
-		res.status(200).send({
-			status: 'success'
+			res.status(200).send({
+				status: 'success',
+			});
+		})
+		.catch((err) => {
+			console.log(`failed to publish message ${err} - ${err.stack}`);
+
+			res.status(500).send(err);
 		});
-	}).catch(err => {
-		console.log(`failed to publish message ${err} - ${err.stack}`);
-
-		res.status(500).send(err);
-	});
 });
 
 app.route('/:subscription/subscribe').get((req, res) => {
-	const subscriber = new Subscriber({
-		project: config.Google.project,
-		pubsub: config.Google.pubsub
-	}, logger);
+	const subscriber = new Subscriber(
+		{
+			project: config.Google.project,
+			pubsub: config.Google.pubsub,
+		},
+		logger
+	);
 
 	subscriber.attachListener(req.params.subscription);
 
 	res.status(200).send({
-		status: 'success'
+		status: 'success',
 	});
 });
 
--- src/logger.js
+++ src/logger.js
@@ -1,49 +1,53 @@
 'use strict';
 
-const Logging = require('@google-cloud/logging');
+const { Logging } = require('@google-cloud/logging');
 const _ = require('lodash');
 
 function Logger(config) {
-    let self = {};
+	let self = {};
 
-    self._config = {};
+	self._config = {};
 
-    self.init = function(config) {
-        self._config = _.merge(self._config, config);
+	self.init = function (config) {
+		self._config = _.merge(self._config, config);
 
-        if (self._config.logger.enabled) {
-            const logging = new Logging({
-                projectId: self._config.project.id
-            });
+		if (self._config.logger.enabled) {
+			const logging = new Logging({
+				projectId: self._config.project.id,
+			});
 
-            self.logger = logging.log(self._config.logger.name);
-            self.logResource = {
-                type: self._config.logger.type,
-            };
-        }
-    }
+			self.logger = logging.log(self._config.logger.name);
+			self.logResource = {
+				type: self._config.logger.type,
+			};
+		}
+	};
 
-    self.log = function(message) {
-        if (self._config.logger.enabled) {
-            const entry = self.logger.entry({
-                resource: self.logResource
-            }, message);
+	self.log = function (message) {
+		if (self._config.logger.enabled) {
+			const entry = self.logger.entry(
+				{
+					resource: self.logResource,
+				},
+				message
+			);
 
-            self.logger.write(entry)
-                .then(() => {
-                    // console.log(`Logged the message to Google Cloud Logs`);
-                })
-                .catch(err => {
-                    // console.error('ERROR:', err);
-                });
-        } else {
-            console.log(`${new Date()} - ${JSON.stringify(message)}`);
-        }
-    }
+			self.logger
+				.write(entry)
+				.then(() => {
+					// console.log(`Logged the message to Google Cloud Logs`);
+				})
+				.catch((err) => {
+					// console.error('ERROR:', err);
+				});
+		} else {
+			console.log(`${new Date()} - ${JSON.stringify(message)}`);
+		}
+	};
 
-    self.init(config);
+	self.init(config);
 
-    return self;
+	return self;
 }
 
 module.exports = Logger;
